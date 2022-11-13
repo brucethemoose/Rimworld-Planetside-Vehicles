22e66f10481ede20785a891339c806b5b1df3579
brucethemoose <AlphaAtlas1@outlook.com>
2022-11-13 14:31:55 -0500

    Start Reaver/ESF stuff

--- Sounds/laundersounds.py
+++ Sounds/laundersounds.py
@@ -1,6 +1,7 @@
 import glob,os
 
-for f in glob.glob("C:\Steam\steamapps\common\RimWorld\Mods\Rimworld-Planetside-Vehicles\Sounds\*.wav"):
+#Losslessly convert weird .wav codecs into 16-bit PCM, so Rimworld can load it.  
+for f in glob.glob("C:\Steam\steamapps\common\RimWorld\Mods\Rimworld-Planetside-Vehicles\Sounds\Air*.wav"):
     os.system('ffmpeg -i "' + f + '" -f wav -acodec pcm_s16le "' + f + '2"')
     os.remove(f)
     os.rename(f + "2", f)
