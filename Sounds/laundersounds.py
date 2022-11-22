ba331edc5c8a446d3a412f735de866820a94af5b
brucethemoose <AlphaAtlas1@outlook.com>
2022-11-21 20:16:59 -0500

    More Colossus Testing

--- Sounds/laundersounds.py
+++ Sounds/laundersounds.py
@@ -1,7 +1,7 @@
 import glob,os
 
 #Losslessly convert weird .wav codecs into 16-bit PCM, so Rimworld can load it.  
-for f in glob.glob("C:\Steam\steamapps\common\RimWorld\Mods\Rimworld-Planetside-Vehicles\Sounds\Colossus_Gecko*.wav"):
+for f in glob.glob("C:\Steam\steamapps\common\RimWorld\Mods\Rimworld-Planetside-Vehicles\Sounds\Shield*.wav"):
     os.system('ffmpeg -i "' + f + '" -f wav -acodec pcm_s16le "' + f + '2"')
     os.remove(f)
     os.rename(f + "2", f)
